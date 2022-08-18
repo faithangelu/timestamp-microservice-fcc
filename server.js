7869faba3e6762d00c018409b8b69a3d6c46a309
Faith Hidalgo <imfaithangelou@gmail.com>
2022-08-18 07:48:23 -0700

    * Initial commit for Timestamp Project

---
+++ server.js
@@ -0,0 +1,38 @@
+const { application } = require('express');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Serving at port...')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+ '/views/index.html');
+})
+
+app.post('/api/:date', (req, res) => {   
+    var date = req.params
+    res.json(req.params);
+})
+
+app.get('/api/', (req, res) => {
+    res.json({ unix: Date.parse(new Date()), utc: new Date().toString()})
+})
+
+app.get('/api/:date', (req, res) => {
+    var date = req.params.date;
+    var dateParam = new Date(date);
+    // var unixTime = Date.parse(date);    
+    
+    if (date === typeof Number) {
+        res.json({ unix: Date.parse(date), utc: dateParam.toString()})   
+        console.log(dateParam)             
+        console.log(typeof dateParam)             
+    } else {
+        res.json({ error: "Invalid Date" })    
+        console.log(1)
+    } 
+})
+// application.get('/api/')
